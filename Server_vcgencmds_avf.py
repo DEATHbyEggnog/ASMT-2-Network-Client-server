366e6c576d2baa5d4e76311c71701e2f47d26c06
DEATHbyEggnog <144825948+DEATHbyEggnog@users.noreply.github.com>
2023-11-23 15:43:22 -0500

    Add files via upload

--- Server_vcgencmds_avf.py
+++ Server_vcgencmds_avf.py
@@ -1,6 +1,7 @@
 import socket
 import os
 import json
+import time
 
 s = socket.socket()
 host = '10.102.13.65'
@@ -8,36 +9,36 @@ port = 5000
 s.bind((host, port))
 s.listen(5)
 
-# gets the Core Temperature from Pi
-measure_temp = os.popen('vcgencmd measure_temp').readline().replace("temp=", "").replace("'C\n", "")
-# Display boolean power
-display_power = os.popen('vcgencmd display_power').readline().strip()
-# Display clock frequency 
-measure_clock = os.popen('vcgencmd measure_clock arm').readline().strip()
-#SDRAM Voltage
-measure_volts = os.popen('vcgencmd measure_volts sdram_c').readline().strip()
+def get_pi_data():
+    # gets the Core Temperature from Pi
+    measure_temp = os.popen('vcgencmd measure_temp').readline().replace("temp=", "").replace("'C\n", "")
+     # Returns a boolean value
+    display_power = os.popen('vcgencmd display_power').readline().strip()
+    # Measure ARM clock frequency
+    measure_clock = os.popen('vcgencmd measure_clock arm').readline().strip()
+    # Measure sdram voltage
+    measure_volts = os.popen('vcgencmd measure_volts sdram_c').readline().strip()  
 
-
-# Dictionary with the data
-data_dict = {
-    "Temperature": measure_temp,
-    "DisplayPower": display_power,
-    "MeasureClock": measure_clock,
-    "MeasureVolts": measure_volts
-}
-
-# Convert dictionary to JSON-formatted string
-json_string = json.dumps(data_dict, indent=2)
+    return {
+        "Temperature": measure_temp,
+        "DisplayPower": display_power,
+        "MeasureClock": measure_clock,
+        "MeasureVolts": measure_volts
+    }
 
 while True:
     c, addr = s.accept()
     print('Got connection from', addr)
 
-    # Print the JSON string
-    print(json_string)
+    # continuously send updated information every 2 seconds
+    while True:
+        data_dict = get_pi_data()
 
-    # Send the JSON string as bytes
-    c.send(json_string.encode('utf-8'))
+        # Convert dictionary to JSON-formatted string
+        json_string = json.dumps(data_dict, indent=2)
 
-    # Close the connection after sending data
-    c.close()
+        # Send the JSON string as bytes
+        c.send(json_string.encode('utf-8'))
+
+        # Wait for a few seconds before sending updated information
+        time.sleep(2)
